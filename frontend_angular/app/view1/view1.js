9837e7148d20ac495e43ce386b1f954088a273b6
nick <nickjungert@googlemail.com>
2021-10-30 13:44:19 +0200

    futzius

--- frontend_angular/app/view1/view1.js
+++ frontend_angular/app/view1/view1.js
@@ -9,6 +9,44 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
 
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+  $scope.searchTerm = "";
+
+  $scope.apiurl = "http://127.0.0.1:8000/api";
+
+  $scope.searchGames = function () {
+      console.log("Search Term: " + $scope.searchTerm);
+
+      var requestUrl = $scope.apiurl + "/app/documents/search/" + $scope.searchTerm;
+
+      console.log(requestUrl);
+
+      $http.get(requestUrl).then(function(response) {
+        $scope.response = response.data;
+        $scope.str1 = angular.fromJson($scope.response);
+        console.log($scope.str1.hits.hits);
+
+        $scope.cnt = 0;
+
+        for(const [key, value] of Object.entries($scope.str1.hits.hits)){
+          console.log(value._source.ResponseName);
+
+          var parent = document.getElementById("1337");
+
+          var node = document.createElement("div");
+          node.id = "game:" + toString($scope.cnt);
+          node.textContent += value._source.ResponseName;
+
+          parent.appendChild(node);
+          $scope.cnt += 1;
+        }
+          var parent = document.getElementById("game:7");
+
+          var node = document.createElement("li");
+          node.id = "futz";
+          node.textContent += "FUTZ";
+
+      });
+  } ;
 }]);
